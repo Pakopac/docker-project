2e34c344462f48e095e17be2875a0e491e77e2e8
lilian <lilian.pacaud1@gmail.com>
2021-09-09 10:16:03 +0200

    add README

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# Project Docker
+# Video Games List
+
+## Presentation:
+Hello, this is a project named <b>video games list</b>, the goal of this project is to have a list of video games based on a <b>database</b> inside an application using <b>FastAPI</b>.
+This project is split in two <b>docker container</b>: application and db, with a docker-compose.
+### APP:
+Web application FastAPI with:
+* Route home on '/'
+* Route video games list on '/list'
+* Get from db with sqlAlchemy
+* Templates with Jinja
+* Run on port 5000
+
+### DATABASE:
+MySQL Database
+
+```
++-----------------------+
+| Tables_in_video_games |
++-----------------------+
+| game                  |
+| game_platform         |
+| game_publisher        |
+| genre                 |
+| platform              |
+| publisher             |
+| region                |
+| region_sales          |
++-----------------------+
+```
+Run on port 3306
+
+## Architecture:
+```
+├── app
+│   ├── app.py
+│   ├── conn.py
+│   ├── Dockerfile
+│   ├── requirements.txt
+│   ├── static
+│   │   └── styles.css
+│   └── templates
+│       └── index.html
+├── db
+│   ├── Dockerfile
+│   └── sql
+│       └── games.sql
+├── docker-compose.yml
+└── README.md
+```
+
+* App Folder
+    * app.py with application
+    * conn.py for sqlAlchemy connextion with db
+    * Dockerfile build by docker-compose
+    * requirements.txt to install modules needed
+    * static with styles files
+    * templates with html files
+* DB folder
+    * Dockerfile build by docker-compose
+    * sql with our database
+* docker-compose to run containers
+* README.md it's me
+
+
+## Install
+First you need to clone the repository:
+```
+git clone git@github.com:Pakopac/docker-project.git
+```
+...
+
--- app/app.py
+++ app/app.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.dialects.mysql import MEDIUMTEXT, MEDIUMBLOB
-import conn
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 import conn
 
@@ -18,7 +21,66 @@ session = Session()
 
 metadata = MetaData()
 
+print('---------------------')
 print('- - - MySQL Docker Container Python connection ok - - - \n')
+print(engine.table_names())
+
+class Game(Base):
+    __tablename__ = 'game'
+    id = Column(Integer, primary_key=True)
+    game_name = Column(String)
+    genre_id = Column(Integer, ForeignKey('genre.id'))
+    game_genre = relationship('Genre')
+
+class Genre(Base):
+    __tablename__ = "genre"
+    id = Column(Integer, primary_key=True)
+    genre_name = Column(String)
+
+
+# Games = Table('game', metadata,
+#     Column('id', Integer, primary_key=True, nullable=False),
+#     Column('genre_id', Integer, ForeignKey('genre.id')),
+#     Column('game_name', String(50)),
+# )
+
+# Genres = Table('genre', metadata,
+#     Column('id', Integer, primary_key=True, nullable=False),
+#     Column('genre_name', String(50)),
+# )
+
+# Publisher = Table('publisher', metadata,
+#     Column('id', Integer, primary_key=True, nullable=False),
+#     Column('publisher_name', String(50)),
+# )
+
+# Game_Publisher = Table('game_publisher', metadata,
+#     Column('id', Integer, primary_key=True, nullable=False),
+#     Column('game_id', Integer),
+#     Column('publisher_id', Integer),
+# )
+
+# Platform = Table('platform', metadata,
+#     Column('id', Integer, primary_key=True, nullable=False),
+#     Column('platform_name', String(50)),
+# )
+
+# Game_Platform = Table('game_platform', metadata,
+#     Column('game_publisher_id', Integer),
+#     Column('platform_id', Integer),
+#     Column('release_year', Integer),
+# )
+
+metadata.create_all(engine)
+
+games_names = session.query(Game.genre_id).all()
+
+print(games_names)
+
+# games = session.query(Games, Genres, Publisher, Platform, Game_Platform).join(Genres, Games.c.genre_id == Genres.c.id).join(Game_Publisher, Games.c.id == Game_Publisher.c.game_id).join(Publisher, Publisher.c.id == Game_Publisher.c.publisher_id).join(Game_Platform, Game_Publisher.c.id == Game_Platform.c.game_publisher_id).join(Platform, Platform.c.id == Game_Platform.c.platform_id).all()
+# games = session.query(Games, Genres, Publisher, Platform, Game_Platform, Game_Publisher).filter(Games.c.genre_id == Genres.c.id, Games.c.id == Game_Publisher.c.game_id, Publisher.c.id == Game_Publisher.c.publisher_id,Game_Publisher.c.id == Game_Platform.c.game_publisher_id, Platform.c.id == Game_Platform.c.platform_id).all()
+
+# print(games)
 
 app = FastAPI()
 
