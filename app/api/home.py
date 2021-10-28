a61feded0e6f55c1cb301b0274cf482c7b9a6150
lilian <lilian.pacaud1@gmail.com>
2021-10-28 15:23:41 +0200

    delete all route

--- app/api/home.py
+++ app/api/home.py
@@ -9,7 +9,3 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-@router.get("/all", response_class=HTMLResponse)
-async def all(request: Request):
-    return templates.TemplateResponse("list.html", {"request": request, "games": games})
